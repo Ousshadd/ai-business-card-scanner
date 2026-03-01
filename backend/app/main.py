8b1b5270aaf889907cf6aaee0824b4af91f8250d
Ousshadd <oussama.haddouche013@gmail.com>
2026-03-01 22:41:30 +0100

    Ajout de l'endpoint /extract avec Mock Data pour les tests mobile

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Hada model dyal l'data lli gha n-rjje3o l'mobile
+class ContactBase(BaseModel):
+    name: str
+    phone: str
+    email: str
+    company: str
+
 @app.get("/")
 def read_root():
-    return {"message": "L'API est opérationnelle ! tu peux maintenant commencer le développement mobile."}
+    return {"message": "Backend is running!"}
 
-@app.get("/health")
-def health_check():
-    # Hna ghadi n-tastiw men be3d wach Tesseract m-installer mzyan
-    return {"status": "ok", "engine": "FastAPI"}
+# HADA HOWA L'ENDPOINT /extract
+@app.post("/extract", response_model=ContactBase)
+async def extract_card_info(file: UploadFile = File(...)):
+    # Hna Oussama gha y-stabel tsswira (Upload)
+    # O hna Adnane gha y-zid l'logic dyal vision.py men be3d
+    
+    # Daba gha n-rjje3o Mock Data (data kdoubiya) bach t-testi l'mobile
+    return {
+        "name": "Oussama Test",
+        "phone": "+212 600-000000",
+        "email": "oussama@example.com",
+        "company": "FSR Rabat"
+    }
